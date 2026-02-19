2fc87e660c1f85800aca60a7015ecf0c4bb00a15
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-19 22:37:36 +0100

    add story model, controller, routes and validation

--- src/routes/storyRoutes.js
+++ src/routes/storyRoutes.js
@@ -1,10 +1,21 @@
-import { Router } from 'express';
-
+import { Router } from "express";
+import { celebrate } from "celebrate";
+import { authenticate } from "../middleware/authenticate.js";
+import { getMyStories } from "../controllers/storyController.js";
+import {
+  createStorySchema,
+  getMyStoriesSchema,
+} from "../validations/storyValidation.js";
 
 const router = Router();
 
-
-router.get('/stories');
+router.get("/stories");
 //...
+router.get(
+  "/myStories",
+  authenticate,
+  celebrate(getMyStoriesSchema),
+  getMyStories,
+);
 
 export default router;
