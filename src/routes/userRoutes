9a4cb569fa97edb27a57c0cf186faa6661f1ec6b
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-02-19 09:08:26 +0200

    feat: add full backend project structure with route and controller stubs (#1)

---
+++ src/routes/userRoutes
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+
+const router = Router();
+
+
+router.get('/stories');
+//...
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+import { errors } from 'celebrate';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import storyRoutes from "./routes/storyRoutes.js";
+
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT ?? 5000;
+
+app.use(logger);
+app.use(express.json({ limit: '5mb' }));
+app.use(cors({ methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'] }));
+app.use(helmet());
+app.use(cookieParser());
+
+app.use(authRoutes);
+app.use(userRoutes);
+app.use(storyRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
