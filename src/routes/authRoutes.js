4e69a6f767acd1df8a995f24e59e01ebac462d88
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-02-19 17:49:08 +0200

    Add /register and /login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
 
 
 const router = Router();
 
 
-router.post('/auth/register');
+router.post("/auth/register", celebrate(registerUserSchema), registerUser);
+router.post("/auth/login", celebrate(loginUserSchema), loginUser);
 //...
 
 export default router;
