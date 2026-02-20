017853d3b40cdd9ee8f742b6f34716aa4af4c1f7
Oleg <luchkoo22@gmail.com>
2026-02-20 09:23:47 +0200

    sav_and_del_user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
-import { Router } from 'express';
-
+import { Router } from "express";
+import { authenticate } from "../middleware/authenticate.js";
+import {
+  addToSaved,
+  removeFromSaved,
+} from "../controllers/userController.js";
 
 const router = Router();
 
-
-// router.get('/stories');
-//...
+router.post("/saved/:storyId", authenticate, addToSaved);
+router.delete("/saved/:storyId", authenticate, removeFromSaved);
 
 export default router;
