cdc0dab552f83fe47e91c377478f8294ab78cedc
Mariia <mariadenkovic4@gmail.com>
2026-02-21 00:45:54 +0200

    add route request reset email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,7 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import { sendEmail } from "../utils/sendMail.js";
 
 export const registerUser = async (req, res) => {
   const { email, password, name } = req.body;
@@ -52,4 +53,42 @@ export const loginUser = async (req, res) => {
   res.status(200).json({
     user,
   });
+};
+
+export const requestResetEmail = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, "User not found"));
+    }
+
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    const hashedToken = await bcrypt.hash(resetToken, 10);
+
+    user.resetPasswordToken = hashedToken;
+    user.resetPasswordExpires = Date.now() + 3600 * 1000;
+    await user.save();
+
+    const message = `
+      Hello ${user.name},
+      You requested a password reset.
+      Click the link below to reset your password:
+      ${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}
+    `;
+
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: "Password Reset Request",
+      text: message,
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: "Password reset email sent successfully",
+    });
+  } catch (err) {
+    next(createHttpError(500, err.message));
+  }
 };
