b5dbfef8245b0296bcd832df0fd3bbf62b45ddc7
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-02-21 13:11:04 +0200

    fix: resolve bugs after merge

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.js";
 
-// import {getAllUsers,getCurrentUser,getUserById,updateUserAvatar,updateUserInfo,} from "../controllers/userController.js";
-// import { celebrate } from "celebrate";
+//import {getAllUsers,getCurrentUser,getUserById,updateUserAvatar,updateUserInfo,} from "../controllers/userController.js";
+import { celebrate } from "celebrate";
 
 const router = Router();
 
 // 1. ПУБЛІЧНИЙ: Отримання всіх авторів + пагінація
-router.get('/users', getAllUsers);
+//router.get('/users', getAllUsers);
 
 // 2. ПРИВАТНИЙ: Інформація про поточного користувача
-router.get('/users/me', authenticate, getCurrentUser);
+//router.get('/users/me', authenticate, getCurrentUser);
 
 // 3. ПУБЛІЧНИЙ: Отримання даних про користувача за ID + його статті
-router.get('/users/:id', getUserById);
+//router.get('/users/:id', getUserById);
 
 // 4. ПРИВАТНИЙ: Оновлення аватару
 // router.patch('/users/me/avatar', authenticate, upload.single('avatar'), updateUserAvatar);
