8ace9e0fa3f4993cb0683b32a5b208574ceb566b
MariiaDe <123482715+MariiaDe@users.noreply.github.com>
2026-02-21 11:01:15 +0200

    add route request reset email (#23)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from '../validations/authValidation.js';
 import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import { registerUser, loginUser, requestResetEmail } from '../controllers/authController.js';
 
 
 const router = Router();
@@ -9,6 +9,7 @@ const router = Router();
 
 router.post("/auth/register", celebrate(registerUserSchema), registerUser);
 router.post("/auth/login", celebrate(loginUserSchema), loginUser);
+router.post("/auth/request-reset-email", celebrate(requestResetEmailSchema), requestResetEmail);
 //...
 
 export default router;
