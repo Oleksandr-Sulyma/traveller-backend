5524e2e4b88642c1ae977a40df739bdd712be45c
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-02-21 07:26:55 +0200

    Added refresh handler

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,19 @@
-import { Router } from 'express';
-import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
-import { celebrate } from 'celebrate';
-import { registerUser, loginUser } from '../controllers/authController.js';
-
+import { Router } from "express";
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from "../validations/authValidation.js";
+import { celebrate } from "celebrate";
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+} from "../controllers/authController.js";
 
 const router = Router();
 
-
 router.post("/auth/register", celebrate(registerUserSchema), registerUser);
 router.post("/auth/login", celebrate(loginUserSchema), loginUser);
-router.post("/auth/refresh", loginUser);
+router.post("/auth/refresh", refreshUserSession);
 
 export default router;
