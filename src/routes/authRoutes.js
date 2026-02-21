3e595784cc6776e83f7c14399d5f6be70a989a89
Daniil <stifflingdagger@gmail.com>
2026-02-21 17:23:26 +0200

    refactor: standardize error messages and improve session handling in authController and authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,29 +1,35 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
-import { 
-  loginUserSchema, 
-  registerUserSchema, 
-  requestResetEmailSchema 
-} from '../validations/authValidation.js';
-import { 
-  registerUser, 
-  loginUser, 
-  refreshUserSession, 
-  requestResetEmail, 
-  logoutUser 
-} from '../controllers/authController.js';
+import {
+  loginUserSchema,
+  registerUserSchema,
+  requestResetEmailSchema,
+} from "../validations/authValidation.js";
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+  requestResetEmail,
+  logoutUser,
+} from "../controllers/authController.js";
 import { authenticate } from "../middleware/authenticate.js";
+import { checkSession } from "../controllers/authController.js";
 
 const router = Router();
 
 router.post("/auth/register", celebrate(registerUserSchema), registerUser);
 router.post("/auth/login", celebrate(loginUserSchema), loginUser);
 router.post("/auth/refresh", refreshUserSession);
-router.post("/auth/request-reset-email", celebrate(requestResetEmailSchema), requestResetEmail);
+router.post(
+  "/auth/request-reset-email",
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 router.post("/auth/logout", authenticate, logoutUser);
 
 // Потрібно буде додати пізніше:
 // router.post("/auth/reset-password", celebrate(resetPasswordSchema), resetPassword);
-// router.get("/auth/check", checkSession);
+
+router.get("/auth/check", checkSession);
 
 export default router;
