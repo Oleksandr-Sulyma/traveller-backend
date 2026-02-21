17b8cb53c5e40c0898e0d262dacbe62daa273a45
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2026-02-21 11:54:58 +0000

    add logout route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 import { loginUserSchema, registerUserSchema, requestResetEmailSchema } from '../validations/authValidation.js';
-import { registerUser, loginUser, refreshUserSession, requestResetEmail } from '../controllers/authController.js';
+import { registerUser, loginUser, refreshUserSession, requestResetEmail, logoutUser } from '../controllers/authController.js';
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.post("/auth/register", celebrate(registerUserSchema), registerUser);
 router.post("/auth/login", celebrate(loginUserSchema), loginUser);
 router.post("/auth/refresh", refreshUserSession);
 router.post("/auth/request-reset-email", celebrate(requestResetEmailSchema), requestResetEmail);
+router.post("/auth/logout", logoutUser);
 //...
 
 export default router;
