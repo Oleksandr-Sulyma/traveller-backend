0a32cf686f07fb5cbe51bece4308e140096f2632
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-02-28 22:18:37 +0200

    Update sessionService.js

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,6 +1,6 @@
 import { Session } from '../models/session.js';
 import createHttpError from 'http-errors';
-import { createSession } from './authServices.js';
+import { createSession } from './AuthServices.js';
 
 export const refreshSessionLogic = async (sessionId, refreshToken) => {
   const session = await Session.findOne({
